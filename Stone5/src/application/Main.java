01b129116b3eb30f8a01135259e1cab46a5ed76f
מורן <@LAPTOP-0R2CQ77E>
2020-06-16 16:56:19 +0300

    First graphical update

--- Stone5/src/application/Main.java
+++ Stone5/src/application/Main.java
@@ -1,10 +1,10 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -12,10 +12,20 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+//			Circle circle1=new Circle();
+//		    circle1.centerXProperty().set(705);
+//		    circle1.centerYProperty().set(170);
+//		    circle1.setRadius(70);
+//		    //circle1.setStroke(Color.RED);
+//		    circle1.setFill(Color.GRAY);
+//		    circle1.getStrokeDashArray().addAll(2d, 21d);
+//		    circle1.setStrokeWidth(3);
+//		    root.getChildren().addAll(circle1);
+			Scene scene = new Scene(root,824,300);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
